b51c31c354b4d0a4fa83c5deb26d549b16c9e164
Javier Bacilio Sanchez <l21140750@queretaro.tecnm.mx>
2024-05-18 15:32:20 -0600

    Game logic implemented

--- src/components/JuegoContainer.vue
+++ src/components/JuegoContainer.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="trash-recognition">
-    <div class="question">
-      <v-img :src="randomImageSrc.image" width="400" height="400" style="display: block; margin: auto;"></v-img>
-      <p style="text-align: center;">¿A qué contenedor pertenece esta basura ({{ randomImageSrc.text }})?</p>
-    </div>
-    <div class="options-container">
-      <div v-for="(option, index) in options" :key="index" class="option" @click="checkAnswer(option,index)">
-        <v-card :color="getColor(index)" width="200" style="overflow-y:auto">
-          <img class="options" :src="option.image" :alt="option.text">
-          <p>{{ option.text }}</p>
-        </v-card>
+    <div v-if="!isGameFinished">
+      <div class="question">
+        <v-row>
+          <v-col align="left">
+            <p class="question-header">{{currentTrashElementIndex+1}}/{{ trashSrc.length }}</p>
+          </v-col>
+          <v-col>
+            <p class="question-header">Aciertos: {{aciertosCount}} - Fallos: {{ fallosCount }}</p>
+          </v-col>
+        </v-row>
+        <v-img :src="currentTrashElement.image" width="400" height="400" style="display: block; margin: auto;"></v-img>
+        <p style="text-align: center;">¿A qué contenedor pertenece esta basura ({{ currentTrashElement.text }})?</p>
       </div>
+      <div class="options-container">
+        <div v-for="(option, index) in options" :key="index" class="option" @click="checkAnswer(option,index)">
+          <v-card :color="getColor(index)" width="200" style="overflow-y:auto">
+            <img class="options" :src="option.image" :alt="option.text">
+            <p>{{ option.text }}</p>
+          </v-card>
+        </div>
+      </div>
+      <v-dialog persistent class="result" v-model="showResult">
+        <v-card>
+          <v-card-title>{{ result }}</v-card-title>
+          <v-card-text>
+            <span v-if="isResultSucess">¡Felicidades! Has acertado la respuesta.</span>
+            <span v-else>¡Lo siento! La respuesta correcta era {{ correctAnswer }}.</span>
+          </v-card-text>
+          <img :src="resultGif" alt="Resultado">
+          <v-card-actions>
+            <v-btn color="secondary" @click="setupNextElement()">Siguiente</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>
-    <div class="result" v-if="showResult">
-      <img :src="resultGif" alt="Resultado">
-      <p>{{ result }}</p>
+    <div v-else>
+      <v-card>
+        <v-card-title>¡Juego terminado!</v-card-title>
+        <v-card-text>
+          <p>Has acertado {{ aciertosCount }} preguntas y has fallado {{ fallosCount }} preguntas.</p>
+          <p>¡Gracias por jugar!</p>
+        </v-card-text>
+      </v-card>
     </div>
   </div>
 </template>
@@ -22,15 +50,12 @@
 <script>
 export default {
   mounted() {
-    this.selectRandomImage();
-    window.addEventListener('beforeunload', this.selectRandomImage);
-  },
-  beforeDestroy() {
-    window.removeEventListener('beforeunload', this.selectRandomImage);
+    this.setup();
   },
   data() {
     return {
-      randomImageSrc:{},
+      currentTrashElement:{},
+      currentTrashElementIndex: 0,
       trashSrc:[
         {text: 'Aceite Quemado', image: require('@/assets/aceite_quemado.jpeg') , categoria: 'LABQUIMICA'},
         {text: 'Botella de Vidrio', image: require('@/assets/botella-vidrio.png'), categoria: 'INORGANICO'},
@@ -48,13 +73,17 @@ export default {
         { text: 'Inorgánico', value: 'INORGANICO' ,image: require('@/assets/pt4.png') },
         { text: 'Laboratorio de Quimica', value: 'LABQUIMICA' ,image: require('@/assets/pt4.png') },
       ],
+      aciertosCount: 0,
+      fallosCount: 0,
       selectedCard: null, // Índice de la carta seleccionada
+      result:'',
       isResultSucess: false, // Indicador de resultado correcto
       resultColor: null, // Color de fondo del resultado
       correctAnswer: null, // Opción correcta
       userAnswer: null, // Respuesta del usuario
       showResult: false, // Indicador para mostrar el resultado
-      resultGif: '' // URL del gif de victoria o derrota
+      resultGif: '', // URL del gif de victoria o derrota
+      isGameFinished: false
     };
   },
   methods: {
@@ -63,20 +92,22 @@ export default {
       this.selectedCard = index 
       if (this.userAnswer === this.correctAnswer) {
         // El usuario acertó la respuesta
+        this.aciertosCount++;
         this.resultColor = 'success' 
         this.isResultSucess = true
+        this.result = '¡Correcto!'
         this.fetchGif('winner')
       } else {
         // El usuario erró la respuesta
+        this.fallosCount++;
         this.fetchGif('loser')
         this.resultColor = 'error'
+        this.result = '¡Incorrecto!'
         this.isResultSucess = false
       }
       this.showResult = true;
-        // Recargar la página después de 5 segundos
-      setTimeout(() => {
-        window.location.reload();
-      }, 5000); // 5000 milisegundos = 5 segundos
+      this.currentTrashElementIndex ++;
+      this.setup();
 
     },
     getColor(index){
@@ -85,10 +116,19 @@ export default {
       }
       return ''; //Para las cards no seleccionadas
     },
-    selectRandomImage() {
-      const randomIndex = Math.floor(Math.random() * this.trashSrc.length);
-      this.randomImageSrc = this.trashSrc[randomIndex];
-      this.correctAnswer = this.trashSrc[randomIndex].categoria
+    setup() {
+      if( this.currentTrashElementIndex < this.trashSrc.length ){
+        this.currentTrashElement = this.trashSrc[this.currentTrashElementIndex];
+        this.correctAnswer = this.trashSrc[this.currentTrashElementIndex].categoria
+      }else{
+        this.isGameFinished = true
+      }
+    },
+    setupNextElement(){
+      this.showResult = false
+      this.selectedCard = null
+      this.resultGif = ''
+      this.getColor(null)
     },
      async fetchGif(searchTerm) {
       const apiKey = 'rs76t7Qf56qdAj8a8sNuYyzkSQ3A88dr';
@@ -98,7 +138,6 @@ export default {
         .then(response => response.json())
         .then(data => {
           this.resultGif = data.data.images.original.url;
-          console.log(this.resultGif)
         })
       }catch(error){
         this.resultGif = null
@@ -141,20 +180,17 @@ export default {
 }
 
 .result {
+  width: 100;
+  transition:dialog-bottom-transition;
   text-align: center;
   font-size: 60px;
   color: #D9EDBF;
   background-color: #90D26D;
   padding-top: 50px;
 }
+.question-header{
+  padding-left: 2rem;
+  font-size:x-large
 
-/*
-verde claro: D9EDBF
-naranja: FF9800
-verde oscuro: 2C7865
-verde medio: 90D26D
-verde claro fosfo: CFFF8D
-beige: FFFFDD
-*/
-
+}
 </style>
