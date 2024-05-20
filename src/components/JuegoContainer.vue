f5b884dfbd58ed853030d55dcb0eee24b89a53b8
ShainaAle <l21140739@queretaro.tecnm.mx>
2024-05-19 23:58:18 -0600

    CSS listo faltan imagenes

--- src/components/JuegoContainer.vue
+++ src/components/JuegoContainer.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="trash-recognition">
-    <div class="question">
-      <v-img :src="randomImageSrc.image" width="400" height="400" style="display: block; margin: auto;"></v-img>
-      <p style="text-align: center;">¿A qué contenedor pertenece esta basura ({{ randomImageSrc.text }})?</p>
-    </div>
-    <div class="options-container">
-      <div v-for="(option, index) in options" :key="index" class="option" @click="checkAnswer(option)">
-        <v-card width="200" style="overflow-y:auto">
-          <img class="options" :src="option.image" :alt="option.text">
-          <p>{{ option.text }}</p>
-        </v-card>
+    <div v-if="!isGameFinished">
+      <div class="question">
+        <v-row>
+          <v-col align="left">
+            <p class="question-header">{{currentTrashElementIndex+1}}/{{ trashSrc.length }}</p>
+          </v-col>
+          <v-col>
+            <p class="question-header">Aciertos: {{aciertosCount}} - Fallos: {{ fallosCount }}</p>
+          </v-col>
+        </v-row>
+        <v-img :src="currentTrashElement.image" width="400" height="400" style="display: block; margin: auto;"></v-img>
+        <p style="text-align: center;">¿A qué contenedor pertenece esta basura ({{ currentTrashElement.text }})?</p>
       </div>
+      <div class="options-container">
+        <div v-for="(option, index) in options" :key="index" class="option" @click="checkAnswer(option,index)">
+          <v-card :color="getColor(index)" class="option-card">
+            <img class="options" :src="option.image" :alt="option.text">
+            <p>{{ option.text }}</p>
+          </v-card>
+        </div>
+      </div>
+      <v-dialog persistent class="result" v-model="showResult">
+        <v-card :class="['result-card', resultColorClass]">
+          <v-card-title class="result-title">{{ result }}</v-card-title>
+          <v-card-text class="result-text">
+            <span v-if="isResultSucess">¡Felicidades! Has acertado la respuesta.</span>
+            <span v-else>¡Lo siento! La respuesta correcta era {{ correctAnswer }}.</span>
+          </v-card-text>
+          <img :src="resultGif" alt="Resultado">
+          <v-card-actions>
+            <v-btn class="btnSig" color="secondary" @click="setupNextElement()">Siguiente</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>
-    <div class="result" v-if="showResult">
-      <img :src="resultGif" alt="Resultado">
-      <p>{{ result }}</p>
+    <div v-else>
+      <v-card>
+        <v-card-title class="result-title">¡Juego terminado!</v-card-title>
+        <v-card-text class="result-text">
+          <p>Has acertado {{ aciertosCount }} preguntas y has fallado {{ fallosCount }} preguntas.</p>
+          <p>¡Gracias por jugar!</p>
+        </v-card-text>
+      </v-card>
     </div>
   </div>
 </template>
@@ -22,85 +50,110 @@
 <script>
 export default {
   mounted() {
-    this.selectRandomImage();
-    window.addEventListener('beforeunload', this.selectRandomImage);
-  },
-  beforeDestroy() {
-    window.removeEventListener('beforeunload', this.selectRandomImage);
+    this.setup();
   },
   data() {
     return {
-      randomImageSrc:{},
-      trashSrc:[
-        {text: 'Aceite Quemado', image: require('@/assets/aceite_quemado.jpeg') , categoria: 'LABQUIMICA'},
-        {text: 'Botella de Vidrio', image: require('@/assets/botella-vidrio.png'), categoria: 'INORGANICO'},
-        {text: 'Botella de Plastico(PET)', image: require('@/assets/botella.png'), categoria: 'PLASTICOS'},
-        {text: 'Bolsa de Frituras', image: require('@/assets/flaming_hot.jpg'), categoria: 'INORGANICO'},
-        {text: 'Manzana', image: require('@/assets/manzana.png'), categoria: 'ORGANICO'},
-        {text: 'Papel', image: require('@/assets/papel.jpg'), categoria: 'ACOPIO'},
-        {text: 'Baterias', image: require('@/assets/pila.png'), categoria: 'LABQUIMICA'},
-        {text: 'Sandwich', image: require('@/assets/sandwich.jpeg'), categoria: 'ORGANICO'},
+      currentTrashElement: {},
+      currentTrashElementIndex: 0,
+      trashSrc: [
+        { text: 'Aceite Quemado', image: require('@/assets/aceite_quemado.jpeg'), categoria: 'LABQUIMICA' },
+        { text: 'Botella de Vidrio', image: require('@/assets/botella-vidrio.png'), categoria: 'INORGANICO' },
+        { text: 'Botella de Plastico(PET)', image: require('@/assets/botella.png'), categoria: 'PLASTICOS' },
+        { text: 'Bolsa de Frituras', image: require('@/assets/flaming_hot.jpg'), categoria: 'INORGANICO' },
+        { text: 'Manzana', image: require('@/assets/manzana.png'), categoria: 'ORGANICO' },
+        { text: 'Papel', image: require('@/assets/papel.jpg'), categoria: 'ACOPIO' },
+        { text: 'Baterias', image: require('@/assets/pila.png'), categoria: 'LABQUIMICA' },
+        { text: 'Sandwich', image: require('@/assets/sandwich.jpeg'), categoria: 'ORGANICO' },
       ],
       options: [
         { text: 'Acopio de papel', value: 'ACOPIO', image: require('@/assets/pt1.png') },
-        { text: 'Orgánicos', value: 'ORGANICO' ,image: require('@/assets/pt2.png') },
-        { text: 'Plásticos', value: 'PLASTICOS' ,image: require('@/assets/pt3.png') },
-        { text: 'Inorgánico', value: 'INORGANICO' ,image: require('@/assets/pt4.png') },
-        { text: 'Laboratorio de Quimica', value: 'LABQUIMICA' ,image: require('@/assets/pt4.png') },
+        { text: 'Orgánicos', value: 'ORGANICO', image: require('@/assets/pt2.png') },
+        { text: 'Plásticos', value: 'PLASTICOS', image: require('@/assets/pt3.png') },
+        { text: 'Inorgánico', value: 'INORGANICO', image: require('@/assets/pt4.png') },
+        { text: 'Laboratorio de Quimica', value: 'LABQUIMICA', image: require('@/assets/pt4.png') },
       ],
-      result: null,
-      correctAnswer: null, // Opción correcta
-      userAnswer: null, // Respuesta del usuario
-      showResult: false, // Indicador para mostrar el resultado
-      resultGif: '' // URL del gif de victoria o derrota
+      aciertosCount: 0,
+      fallosCount: 0,
+      selectedCard: null,
+      result: '',
+      isResultSucess: false,
+      resultColor: null,
+      correctAnswer: null,
+      userAnswer: null,
+      showResult: false,
+      resultGif: '',
+      isGameFinished: false
     };
   },
+  computed: {
+    resultColorClass() {
+      return this.isResultSucess ? 'success-text' : 'error-text';
+    }
+  },
   methods: {
-    checkAnswer(option) {
+    checkAnswer(option, index) {
       this.userAnswer = option.value;
+      this.selectedCard = index;
       if (this.userAnswer === this.correctAnswer) {
-        // El usuario acertó la respuesta
-        this.result = 'ACERTASTE!' 
-        this.fetchGif('winner')
+        this.aciertosCount++;
+        this.resultColor = 'success';
+        this.isResultSucess = true;
+        this.result = '¡Correcto!';
+        this.fetchGif('winner');
       } else {
-        // El usuario erró la respuesta
-        this.fetchGif('loser')
-        this.result = 'FALLASTE!' 
+        this.fallosCount++;
+        this.fetchGif('loser');
+        this.resultColor = 'error';
+        this.result = '¡Incorrecto!';
+        this.isResultSucess = false;
       }
       this.showResult = true;
-        // Recargar la página después de 5 segundos
-      setTimeout(() => {
-        window.location.reload();
-      }, 5000); // 5000 milisegundos = 5 segundos
-
+      this.currentTrashElementIndex++;
+      this.setup();
     },
-    selectRandomImage() {
-      const randomIndex = Math.floor(Math.random() * this.trashSrc.length);
-      this.randomImageSrc = this.trashSrc[randomIndex];
-      this.correctAnswer = this.trashSrc[randomIndex].categoria
+    getColor(index) {
+      if (index == this.selectedCard) {
+        return this.isResultSucess ? 'success' : 'error';
+      }
+      return '';
     },
-     async fetchGif(searchTerm) {
+    setup() {
+      if (this.currentTrashElementIndex < this.trashSrc.length) {
+        this.currentTrashElement = this.trashSrc[this.currentTrashElementIndex];
+        this.correctAnswer = this.trashSrc[this.currentTrashElementIndex].categoria;
+      } else {
+        this.isGameFinished = true;
+      }
+    },
+    setupNextElement() {
+      this.showResult = false;
+      this.selectedCard = null;
+      this.resultGif = '';
+      this.getColor(null);
+    },
+    async fetchGif(searchTerm) {
       const apiKey = 'rs76t7Qf56qdAj8a8sNuYyzkSQ3A88dr';
       const endpoint = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${searchTerm}`;
-      try{
-      await fetch(endpoint)
-        .then(response => response.json())
-        .then(data => {
-          this.resultGif = data.data.images.original.url;
-          console.log(this.resultGif)
-        })
-      }catch(error){
-        this.resultGif = null
+      try {
+        await fetch(endpoint)
+          .then(response => response.json())
+          .then(data => {
+            this.resultGif = data.data.images.original.url;
+          });
+      } catch (error) {
+        this.resultGif = null;
       }
+    }
   }
-}};
+};
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap');
 
 .trash-recognition {
-  background: linear-gradient(to bottom,#FFFFDD ,  #CFFF8D);
+  background: linear-gradient(to bottom, #FFFFDD, #CFFF8D);
   font-family: 'Poetsen One', sans-serif;
   color: #2C7865; /* Color de texto principal */
   font-size: 40px;
@@ -121,8 +174,9 @@ export default {
 }
 
 .options {
-  width: 200px;
+  width: 350px;
   height: 200px;
+  margin: auto;
 }
 
 .option {
@@ -130,6 +184,8 @@ export default {
 }
 
 .result {
+  width: 100;
+  transition: dialog-bottom-transition;
   text-align: center;
   font-size: 60px;
   color: #D9EDBF;
@@ -137,13 +193,41 @@ export default {
   padding-top: 50px;
 }
 
-/*
-verde claro: D9EDBF
-naranja: FF9800
-verde oscuro: 2C7865
-verde medio: 90D26D
-verde claro fosfo: CFFF8D
-beige: FFFFDD
-*/
+.question-header {
+  padding-left: 2rem;
+  font-size: x-large;
+}
+
+.option-card {
+  overflow-y: auto;
+  text-align: center;
+  font-size: 30px; /* Tamaño de la fuente para las opciones */
+}
+
+.result-card {
+  background-color: #fbcd59 !important; /* Fondo de la tarjeta de resultados */
+  color: #2C7865 !important; /* Color de texto */
+  padding: 20px !important;
+  text-align: center !important;
+}
+
+.success-text {
+  color: green !important;
+}
+
+.error-text {
+  color: red !important;
+}
+
+.result-title {
+  font-size: 70px !important;
+  margin-bottom: 50px; /* Separación entre el título y el texto */
+  text-align: center!important;
+}
+
+.result-text {
+  font-size: 30px !important;
+  margin-bottom: 60px; /* Separación entre el texto y el botón */
+}
 
 </style>
